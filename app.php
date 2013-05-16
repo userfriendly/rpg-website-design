df08892bd6816a5f3e9d1912b8aadf157f8eb359
Moritz Kraft <chamalla.extract@gmail.com>
2013-05-16 01:12:03 +0100

    added placeholder pics and texts

--- app.php
+++ app.php
@@ -2,6 +2,7 @@
 <?php /* seamless paper by http://goblinstock.deviantart.com/ */ ?>
 <html>
     <head>
+        <meta name="robots" content="noindex, nofollow" />
         <title><?php echo $title; ?></title>
         <link href='http://fonts.googleapis.com/css?family=Asul' rel='stylesheet' type='text/css' />
         <link href='http://fonts.googleapis.com/css?family=Tangerine' rel='stylesheet' type='text/css' />
@@ -12,47 +13,31 @@
                 margin: 0;
                 font-family: 'Asul', serif;
             }
-            /*'''''''''''*\
-            |    FONTS    |
-            \*___________*/
-            h1 {
-                font-family: 'Pirata One', serif;
-                font-size: 48pt;
-                text-shadow: 0 0 12px rgba( 170, 150, 130, 1 );
-            }
-            h2 {
-                font-family: 'Tangerine', serif;
-                font-size: 2em;
-            }
-            a {
-                color: black;
-            }
-            span.horizontal-separator {
-                margin: 0 8px;
-            }
             /*''''''''''''*\
             |    LAYOUT    |
             \*____________*/
             body {
+                background-color: #090f03;
                 background-image: url( '/images/the-hub-of-time-by-daniel-dociu.jpg' );
-                background-repeat: repeat-y;
+                background-repeat: no-repeat;
                 background-position: center -300px;
             }
             div.bodywrapper {
                 width: 960px;
                 margin: 0 auto 40px;
                 color: white;
-                text-shadow: 1px 1px 1px rgba( 0, 0, 0, 0.3 );
             }
             div.page {
                 border-radius: 3px;
                 background-color: rgba( 255, 255, 255, 0.5 );
                 background-image: url( '/images/seamless-paper.png' );
-                padding: 20px;
+                padding: 10px 20px 20px;
                 box-shadow: inset 1px 1px 0 rgba( 255, 255, 255, 0.3 ),
                             inset -1px 0 0 rgba( 255, 255, 255, 0.2 ),
                             inset 0 -1px 1px rgba( 0, 0, 0, 0.5 ),
-                            0 0 5px rgba( 0, 0, 0, 0.5 );
+                            0 0 3px rgba( 0, 0, 0, 0.3 );
+                text-shadow: 1px 1px 1px rgba( 0, 0, 0, 0.3 );
+                
             }
             div.footerwrapper {
                 position: fixed;
@@ -69,43 +54,124 @@
                 font-size: 8pt;
                 text-shadow: -1px 1px 1px white;
             }
+            img.textwrap {
+                margin-top: 5px;
+                margin-bottom: 5px;
+            }
+            img.textwrap.left {
+                float: left;
+                margin-right: 15px;
+            }
+            img.textwrap.right {
+                float: right;
+                margin-left: 15px;
+            }
+            img.character {
+                width: 143px;
+                height: 200px;
+            }
+            /*'''''''''''*\
+            |    FONTS    |
+            \*___________*/
+            div.title {
+                font-family: 'Pirata One', serif;
+                font-size: 48pt;
+                text-shadow: 0 0 12px rgba( 170, 150, 130, 1 );
+            }
+            h2 {
+                font-family: 'Tangerine', serif;
+                font-size: 2em;
+                margin: 10px 0 6px;
+            }
+            p {
+              text-indent: 1.5em;
+              margin: 10px 0;
+            }
+            p.blurb {
+              font-style: italic;
+              margin: 10px 0;
+            }
+            a {
+                color: black;
+            }
+            span.horizontal-separator {
+                margin: 0 8px;
+            }
         </style>
     </head>
     <body>
         <div class="bodywrapper">
             <div class="header">
-                <h1><?php echo $title; ?></h1>
+                <div class="title"><?php echo $title; ?></div>
             </div>
             <div class="page">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <h2>This is a headline</h2>
-                <p>Hodor hodor hodor. Hodor. Hodor hodor.</p>
-                <p>Hodor. Hodor hodor. Hodor? Hodor.</p>
-                <h2>This is another headline</h2>
-                <p>Hodor hodor hodor. Hodor. Hodor hodor.</p>
-                <p>Hodor. Hodor hodor. Hodor? Hodor.</p>
-                <h2>This is also a headline</h2>
-                <p>Hodor hodor hodor. Hodor. Hodor hodor.</p>
-                <p>Hodor. Hodor hodor. Hodor? Hodor.</p>
-                <h2>This is a headline still</h2>
-                <p>Hodor hodor hodor. Hodor. Hodor hodor.</p>
-                <p>Hodor. Hodor hodor. Hodor? Hodor.</p>
-                <h2>This is also a headline</h2>
-                <p>Hodor hodor hodor. Hodor. Hodor hodor.</p>
-                <p>Hodor. Hodor hodor. Hodor? Hodor.</p>
-                <h2>This is a headline still</h2>
-                <p>Hodor hodor hodor. Hodor. Hodor hodor.</p>
-                <p>Hodor. Hodor hodor. Hodor? Hodor.</p>
+                <p class="blurb">
+                    "Oh, I know. You're from the future. And you came here to
+                    change, prevent, or cause a row of incredibly
+                    important events.
+                    And I'm supposed to be surprised or incredulous or
+                    some such thing.
+                    Am I right? Of course I'm right.
+                    Well, let me tell you something. You people have no idea how
+                    incredibly annoying you are.
+                    In any given timeline, somewhere at some point, some
+                    nosey half-wit
+                    will find out how to travel back in time - and then
+                    what? Where do you think they end up? Huh?
+                    Who's going to clean up the mess they create?
+                    Yeah,
+                    I'm sure it sounds all totally dandy to you
+                    - hey, let's
+                    manipulate this, change that. Save this
+                    girl, kill that guy.
+                    Well, guess what, Robinson? Not you, not today.
+                    You're nicked!
+                </p>
+                <p class="blurb">
+                    ... Who the fuck is Adolf Hinkel anyway?"
+                </p>
+                <h1>Setting</h1>
+                <p>
+                    Alternate history Europe, mostly London,
+                    between 1900 and 1910.
+                </p>
+                <h1>Characters</h1>
+                <img class="character textwrap right" src="/images/tsiolkovsky.jpg" />
+                <h2>Konstantin Tsiolkovsky</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <img class="character textwrap left" src="/images/hedda.jpg" />
+                <h2>Hedda Blunt-Lytton</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <br style="clear: both;" />
             </div>
         </div>
         <div class="footerwrapper">
             <div class="footer">
                 <strong><i><?php echo $title; ?></i></strong>
                 &nbsp;
-                is a private non-profit role-playing game
+                is a private invitation-only role-playing game
                 <span class="horizontal-separator">&bull;</span>
                 Website design by
                 <a href="http://www.writtengames.com">Written Games</a>
